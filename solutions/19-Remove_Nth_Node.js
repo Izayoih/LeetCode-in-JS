9346faa3810aa56d6bdc33ff4159609a60b04395
Izayoih <izayoih0039@gmail.com>
2017-11-02 22:33:56 +0800

    update: 17,19

---
+++ solutions/19-Remove_Nth_Node.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+  var length = 1
+  var p = head
+  if (p.next === null) {
+      return null
+  }
+  while (p.next) {
+      length ++
+      p = p.next
+  }
+  var index = length - n
+  p = head
+  while (index - 1 > 0) {
+      p = p.next
+      index--
+  }
+  if (index === 0) {
+      p = p.next
+      head.next = null
+      return p
+  }
+  var pre = p
+  var next = p.next.next
+  pre.next = next
+  return head
+}
